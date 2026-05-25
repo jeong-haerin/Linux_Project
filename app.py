e8751859f1ea5412c5f00266e230507c98780916
jeong-haerin <jeonghaerin@ewha.ac.kr>
2026-05-25 23:14:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/database/<path:filename>")
+def database_file(filename):
+    return send_from_directory("database", filename)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
